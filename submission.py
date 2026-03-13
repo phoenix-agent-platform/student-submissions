37935b67e4415f41bf10b9da19fefaf09165a3f1
Khai Hoang <uka.pgr@gmail.com>
2026-03-13 14:41:56 +0700

    update v7: trigger new run

--- submission.py
+++ submission.py
@@ -1,13 +1,4 @@
-# Student Submission v6
-
+# v7
 def hello(name: str) -> str:
-    if not name:
-        raise ValueError("name cannot be empty")
     return f"Hello, {name}\!"
-
-def add(a: int, b: int) -> int:
-    return a + b
-
-if __name__ == "__main__":
-    print(hello("World"))
-    print(add(1, 2))
+print(hello("World"))
